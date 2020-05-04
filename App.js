31b6ba3174105217dce3f52cb11cfffb928a6ac7
adludo <alex@alexs-air-8.home>
2020-05-05 01:33:44 +1000

    4:20:59

---
+++ App.js
@@ -0,0 +1,30 @@
+
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import Header from './components/Header';
+import StartGameScreen from './screens/StartGameScreen';
+
+export default function App() {
+  return (
+    <View style={styles.screen}>
+      <Header title="Guess a number" />
+      <StartGameScreen />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+  }
+});
+
