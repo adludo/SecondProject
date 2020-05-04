b0ea38f99a6b797b6406e7d112185c1479d94c93
adludo <alex@alexs-air-8.home>
2020-05-05 01:28:29 +1000

    4:20:59

--- SecondBasicProject/App.js
+++ SecondBasicProject/App.js
@@ -10,12 +10,21 @@ import {
   StatusBar,
 } from 'react-native';
 
+import Header from './components/Header';
+import StartGameScreen from './screens/StartGameScreen';
 
 export default function App() {
-  
+  return (
+    <View style={styles.screen}>
+      <Header title="Guess a number" />
+      <StartGameScreen />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-
+  screen: {
+    flex: 1,
+  }
 });
 
---
+++ SecondBasicProject/screens/StartGameScreen.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { View, StyleSheet, Text, TextInput, Button } from 'react-native';
+
+import Card from '../components/Card';
+
+const StartGameScreen = props => {
+    return (
+        <View style={styles.screen}>
+            <Text style={styles.title}> Start a new game </Text>
+            <Card>
+                <View style={styles.inputContainer}>
+                    <Text>Select a number</Text>
+                    <TextInput />
+                    <View style={styles.buttonContainer}>
+                        <Button title="Reset" onPress={() => { }} />
+                        <Button title="Reset" onPress={() => { }} />
+                    </View>
+                </View>
+            </Card>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        padding: 10,
+        alignItems: 'center',
+    },
+    title: {
+        fontSize: 20,
+        marginVertical: 10,
+    },
+    inputContainer: {
+        width: 300,
+        maxWidth: '80%',
+        alignItems: 'center',
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        width: '100%',
+        justifyContent: 'space-between',
+        paddingHorizontal: 15,
+    },
+});
+
+export default StartGameScreen;
